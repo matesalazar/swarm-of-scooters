46a23a5e22a8a2921d8d8731180df7265ebbc8c4
Kevin Amézaga <kevin@mostlikelykevin.com>
2021-05-01 15:37:01 -0400

    Update init.R

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-my_packages <- c("httr","sf","leaflet","jsonlite","tidyverse","data.table","units","htmltools","lwgeom","shinydashboard","shiny")
+my_packages <- c("udunits2","units","httr","sf","leaflet","jsonlite","tidyverse","data.table","htmltools","lwgeom","shinydashboard","shiny")
  install_if_missing <- function(p) {
  if(p %in% rownames(installed.packages())==FALSE){
  install.packages(p)}
