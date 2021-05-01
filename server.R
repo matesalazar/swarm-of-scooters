4c844c84adf9f87ddd9d3c6d2e999b0b86f9c488
Kevin Amézaga <kevin@mostlikelykevin.com>
2021-05-01 19:02:27 -0400

    Update server.R

--- server.R
+++ server.R
@@ -63,7 +63,7 @@ getDocklessDevices <- function (provider, url) {
       print('limeplaceholderlogic')
     } else if(provider=='cyclehop'){
       rdf <- rdf %>% mutate(vehicle_type=if_else(is_ebike==1,'ebike','bike'))
-    } else if(provider %in% c('lyft')){
+    } else if(provider %in% c('lyft','helbiz')){
       rdf <- rdf %>% mutate(vehicle_type=if_else(type=='electric_scooter','scooter','ebike'))
     }
     # TODO: add SPIN
